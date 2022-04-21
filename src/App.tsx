2e1f3703eee6a1c8e13891c19404bab45782f030
Pablo Penalva <penalva.pablo@gmail.com>
2022-04-21 13:32:50 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import paper from './images/icon-paper.svg';
+import scissors from './images/icon-scissors.svg';
+import rock from './images/icon-rock.svg';
 
 function App() {
+  const [score, setScore] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gradient">
+      {/* Header */}
+      <div className="header">
+        <h1 className="header__title">ROCK PAPER SCISSORS</h1>
+        <div className="haeder__scoreBox">
+          <h2 className="header__scoreBox__title">Score</h2>
+          <p className="header__scoreBox__score">{score}</p>
+        </div>
+      </div>
+      {/* Game */}
+      <div className="game">
+        <img src={paper} alt="paper" className="icon icon__paper" />
+        <img src={scissors} alt="scissors" className="icon icon__scissors" />
+        <img src={rock} alt="rock" className="icon icon__rock" />
+      </div>
+      <button className="rules__btn">rules</button>
     </div>
   );
 }
